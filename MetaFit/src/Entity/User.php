25609522a1b7813bb03cfead4b4576d05daf3449
Daniel Jiménez Urbano <danielji55@gmail.com>
2026-02-04 20:53:44 +0100

    Hechos Login/Register

--- MetaFit/src/Entity/User.php
+++ MetaFit/src/Entity/User.php
@@ -6,12 +6,14 @@ use App\Entity\Subscription;
 use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
